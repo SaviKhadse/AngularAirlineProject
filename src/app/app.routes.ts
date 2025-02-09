88b3ab06fd38d2d753f7341dca4455a6081d42c4
SaviKhadse <savitakhadse@gmail.com>
2025-02-09 03:26:58 -0800

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UsersComponent } from './pages/users/users.component';
+import { BookingComponent } from './pages/booking/booking.component';
+import { FlightsComponent } from './pages/flights/flights.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, // Default page
+    { path: 'users', component: UsersComponent },
+    { path: 'flights', component: FlightsComponent },
+    { path: 'booking', component: BookingComponent },
+];
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    phone: string;
+    createdAt: string;
+    updatedAt: string;
+    version: number;
+}
